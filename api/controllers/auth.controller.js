1591a74c3ff590fbb9da5fd7229d00a0ae9211d3
Robots-Pixels <robotspixels@gmail.com>
2024-12-10 09:26:56 +0100

    Just reformatting text

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -35,7 +35,10 @@ export const signin = async (req, res, next) => {
         const {password: pass, ...rest} = validUser._doc;
 
         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
-        res.cookie('access_token', token, {httpOnly:true}).status(200).json({rest});
+        res
+        .cookie('access_token', token, {httpOnly:true})
+        .status(200)
+        .json({rest});
 
     } catch (error) {
         next(error);
